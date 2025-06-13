ef82bf2d1a1514a755ddde11a5db3082204462d2
Santiago Lamot <164097782+SantiagoLamot@users.noreply.github.com>
2025-06-13 15:25:09 -0300

    Update ApiMpApplication.java

--- src/main/java/com/example/API_MP/ApiMpApplication.java
+++ src/main/java/com/example/API_MP/ApiMpApplication.java
@@ -2,8 +2,10 @@ package com.example.API_MP;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class ApiMpApplication {
 
 	public static void main(String[] args) {
