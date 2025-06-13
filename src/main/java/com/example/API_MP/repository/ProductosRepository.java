4c062d4a88c1405541bb92b116639ecba84fb52e
Santiago Lamot <164097782+SantiagoLamot@users.noreply.github.com>
2025-06-12 23:06:52 -0300

    Update ProductosRepository.java

--- src/main/java/com/example/API_MP/repository/ProductosRepository.java
+++ src/main/java/com/example/API_MP/repository/ProductosRepository.java
@@ -1,8 +1,11 @@
 package com.example.API_MP.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.API_MP.entidades.Productos;
 
 public interface ProductosRepository extends JpaRepository<Productos, Long> {
+    public List<Productos> findByReservadoTrue();
 }
