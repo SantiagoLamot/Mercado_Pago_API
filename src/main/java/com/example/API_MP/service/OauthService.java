325f2e058479b1e3df77c2b1d46a2cf21a175e02
Santiago Lamot <164097782+SantiagoLamot@users.noreply.github.com>
2025-06-14 02:06:51 -0300

    Update OauthService.java

--- src/main/java/com/example/API_MP/service/OauthService.java
+++ src/main/java/com/example/API_MP/service/OauthService.java
@@ -1,7 +1,9 @@
 package com.example.API_MP.service;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 
+@Service
 public class OauthService {
     @Value("${clientId}")
     String clientId;
