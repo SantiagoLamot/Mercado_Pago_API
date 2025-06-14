801f557ab94b702b2c73a2d7a21fd6447b60e9d3
Santiago Lamot <164097782+SantiagoLamot@users.noreply.github.com>
2025-06-14 01:44:33 -0300

    Update OauthController.java

--- src/main/java/com/example/API_MP/controller/OauthController.java
+++ src/main/java/com/example/API_MP/controller/OauthController.java
@@ -1,10 +1,25 @@
 package com.example.API_MP.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.API_MP.service.OauthService;
+
 
 @RestController
 @RequestMapping("oauth")
 public class OauthController{
+    OauthService oauthService;
+
+    public OauthController(OauthService os){
+        this.oauthService = os;
+    }
+
+    @GetMapping("init")
+    public String getMethodName(@RequestParam String param) {
+        return oauthService.UrlAutorizacion();
+    }
     
 }
