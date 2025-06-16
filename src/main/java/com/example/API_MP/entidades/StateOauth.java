28c02d1c7f5f92d685c880ed9b46aea90c66c5ca
Santiago Lamot <164097782+SantiagoLamot@users.noreply.github.com>
2025-06-16 13:04:46 -0300

    Update StateOauth.java

--- src/main/java/com/example/API_MP/entidades/StateOauth.java
+++ src/main/java/com/example/API_MP/entidades/StateOauth.java
@@ -4,6 +4,8 @@ import java.time.LocalDateTime;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
@@ -15,13 +17,17 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class StateOauth {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
+    
     @NotNull
     @Column(name= "id_usuario")
     Long idUsuario;
+    
     @NotNull
     String state;
     LocalDateTime creado = LocalDateTime.now();
+    
     public StateOauth(Long id, String state){
         this.idUsuario = id;
         this.state = state;
