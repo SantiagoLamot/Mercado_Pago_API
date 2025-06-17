dfd9e3118dacb9c30c72b80b6491b489972f6396
Santiago Lamot <164097782+SantiagoLamot@users.noreply.github.com>
2025-06-17 16:19:04 -0300

    Update StateOauthService.java

--- src/main/java/com/example/API_MP/service/StateOauthService.java
+++ src/main/java/com/example/API_MP/service/StateOauthService.java
@@ -4,16 +4,23 @@ import org.springframework.stereotype.Service;
 
 import com.example.API_MP.entidades.StateOauth;
 import com.example.API_MP.repository.StateOauthRepository;
+
 @Service
 public class StateOauthService {
     private final StateOauthRepository stateOauthRepository;
 
-    public StateOauthService(StateOauthRepository stateOauthRepository){
+    public StateOauthService(StateOauthRepository stateOauthRepository) {
         this.stateOauthRepository = stateOauthRepository;
     }
 
-    public void guardarStateOauth(Long idUsuarioLogueado,String state){
+    public void guardarStateOauth(Long idUsuarioLogueado, String state) {
         StateOauth entity = new StateOauth(idUsuarioLogueado, state);
         stateOauthRepository.save(entity);
+    }
+
+    public Long obtenerIdUsuarioPorState(String state) {
+        return stateOauthRepository.findByState(state)
+                .orElseThrow(() -> new RuntimeException("state no encontrado"))
+                .getId();
     }
 }
