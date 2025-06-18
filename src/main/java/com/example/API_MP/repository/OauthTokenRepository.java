f575284d4a04d3f828edf13a67a1cad79c675b55
Santiago Lamot <164097782+SantiagoLamot@users.noreply.github.com>
2025-06-18 15:25:28 -0300

    Update OauthTokenRepository.java

--- src/main/java/com/example/API_MP/repository/OauthTokenRepository.java
+++ src/main/java/com/example/API_MP/repository/OauthTokenRepository.java
@@ -1,9 +1,12 @@
 package com.example.API_MP.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.API_MP.entidades.OauthToken;
 
+
 public interface OauthTokenRepository extends JpaRepository<OauthToken, Long>{
-    
+    Optional<OauthToken> findByUsuarioId(Long userId);
 }
