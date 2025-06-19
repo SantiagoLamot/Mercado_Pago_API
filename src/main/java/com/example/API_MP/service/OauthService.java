344c83a5e39972ce585a35c7f7288d52274a844a
Santiago Lamot <164097782+SantiagoLamot@users.noreply.github.com>
2025-06-19 16:24:02 -0300

    Update OauthService.java

--- src/main/java/com/example/API_MP/service/OauthService.java
+++ src/main/java/com/example/API_MP/service/OauthService.java
@@ -1,6 +1,7 @@
 package com.example.API_MP.service;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Value;
@@ -117,5 +118,9 @@ public class OauthService {
             }
             return false;
         }
+    }
+
+    public List<OauthToken> obtenerTokenDeUsuariosVendedores(){
+        return oauthRepository.findByUsuario_VendedorTrue();
     }
 }
