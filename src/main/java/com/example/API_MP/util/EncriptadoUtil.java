4a25aabcceb81be160d485048130acbd3fc5ae8d
Santiago Lamot <164097782+SantiagoLamot@users.noreply.github.com>
2025-06-21 00:29:12 -0300

    Update EncriptadoUtil.java

--- src/main/java/com/example/API_MP/util/EncriptadoUtil.java
+++ src/main/java/com/example/API_MP/util/EncriptadoUtil.java
@@ -1,5 +1,16 @@
 package com.example.API_MP.util;
+import org.springframework.beans.factory.annotation.Value;
 
 public class EncriptadoUtil {
+    @Value("${algoritmoEncriptar}")
+    private static String algoritmoEncriptar;
+    
+    @Value("${secretKeyEncriptar}")
+    private static String secretKeyEncriptar;
 
+    public static String encriptar(String plainText) {
+    }
+
+    public static String desencriptar(String encryptedText) {
+    }
 }
