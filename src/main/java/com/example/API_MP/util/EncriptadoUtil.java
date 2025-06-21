1ec3c1685a09612ec9a8ade7c5c2a2b2359c920f
Santiago Lamot <164097782+SantiagoLamot@users.noreply.github.com>
2025-06-21 00:29:35 -0300

    Update EncriptadoUtil.java

--- src/main/java/com/example/API_MP/util/EncriptadoUtil.java
+++ src/main/java/com/example/API_MP/util/EncriptadoUtil.java
@@ -1,4 +1,9 @@
 package com.example.API_MP.util;
+import java.util.Base64;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+
 import org.springframework.beans.factory.annotation.Value;
 
 public class EncriptadoUtil {
@@ -9,8 +14,18 @@ public class EncriptadoUtil {
     private static String secretKeyEncriptar;
 
     public static String encriptar(String plainText) {
+        try {
+            SecretKeySpec secretKey = new SecretKeySpec(secretKeyEncriptar.getBytes(), algoritmoEncriptar);
+            Cipher cipher = Cipher.getInstance(algoritmoEncriptar);
+            cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+            byte[] encryptedBytes = cipher.doFinal(plainText.getBytes());
+            return Base64.getEncoder().encodeToString(encryptedBytes);
+        } catch (Exception e) {
+            throw new RuntimeException("Error al cifrar el token", e);
+        }
     }
 
     public static String desencriptar(String encryptedText) {
+   
     }
 }
