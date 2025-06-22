6e446dcc6215cbb4c67af8291a81a1fd513e4d05
Santiago Lamot <164097782+SantiagoLamot@users.noreply.github.com>
2025-06-22 19:51:27 -0300

    Update OauthController.java

--- src/main/java/com/example/API_MP/controller/OauthController.java
+++ src/main/java/com/example/API_MP/controller/OauthController.java
@@ -1,5 +1,6 @@
 package com.example.API_MP.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,25 +9,37 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.API_MP.service.OauthService;
 
-
 @RestController
 @RequestMapping("/oauth")
-public class OauthController{
+public class OauthController {
     OauthService oauthService;
 
-    public OauthController(OauthService os){
+    public OauthController(OauthService os) {
         this.oauthService = os;
     }
 
     @GetMapping("/init")
-    public String init() {
-        return oauthService.UrlAutorizacion();
+    public ResponseEntity<String> init() {
+        try{
+            return ResponseEntity.ok(oauthService.UrlAutorizacion());
+        }
+        catch(Exception e){
+            return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body("Error al crear URL de autenticación: " + e.getMessage());
+        }
     }
+
     @GetMapping("/callback")
-    public ResponseEntity<String> callback(@RequestParam("code") String code,
-    @RequestParam("state") String state) {
-        String resultado = oauthService.obtenerAccessToken(code, state);
-        return ResponseEntity.ok(resultado);
+    public ResponseEntity<String> callback(@RequestParam("code") String code, @RequestParam("state") String state) {
+        try {
+            oauthService.obtenerAccessToken(code, state);
+            return ResponseEntity.ok("Autenticación con Mercado Pago completada correctamente.");
+        } catch (RuntimeException e) {
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body("Error al completar la autenticación: " + e.getMessage());
+        }
     }
-    
+
 }
